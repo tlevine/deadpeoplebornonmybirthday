83f5c3c15c4415dcd6c915362ea92907229acd85
Thomas Levine <occurrence@thomaslevine.com>
2012-06-17 14:24:30 -0400

    aoeu

--- api/_birthdayloop.py
+++ api/_birthdayloop.py
@@ -6,22 +6,21 @@ MAX_BIRTHDAY = datetime.date(2011, 1, 1)
 
 def main(func, wildyears = False, wilddays = False):
 
-def _do_wild_day_of_month(func, year, month,
-        mock_query_result = None):
+def _do_wild_day_of_month(func, year, month, *args, *kwargs):
     min_birthday = datetime.date(year, month, 1)
     max_birthday = min_birthday + timedelta(months = 1) - timedelta(days = 1)
-    _do_range(func, min_birthday, max_birthday)
-        
-def _do_wild_year(func, mock_query_result = None):
-    min_birthday = datetime.date(2008, 1, 1)
-    max_birthday = datetime.date(2009,12,31)
-    results_raw = _do_range(func, min_birthday, max_birthday)
+    results_raw _do_range(func, min_birthday, max_birthday, *args, *kwargs)
     # Convert to wildcard results
     return results_wild
         
-def _do_range(func,
-        min_birthday, max_birthday,
-        mock_query_result = None):
+def _do_wild_year(func, *args, *kwargs):
+    min_birthday = datetime.date(2008, 1, 1)
+    max_birthday = datetime.date(2009,12,31)
+    results_raw = _do_range(func, min_birthday, max_birthday, *args, *kwargs)
+    # Convert to wildcard results
+    return results_wild
+        
+def _do_range(func, min_birthday, max_birthday, mock_query_result = None):
     'Perform some function, reporting a list of results bay date.'
     current_birthday = copy(min_birthday)
     while current_birthday < max_birthday:
