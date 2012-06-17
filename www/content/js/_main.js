da8c6e24fe73258911030a2dbc2db1caf13252e4
Thomas Levine <occurrence@thomaslevine.com>
2012-06-17 17:29:46 -0400

    aoeu

--- www/content/js/_main.js
+++ www/content/js/_main.js
@@ -1,3 +1,11 @@
-/* Author:
+/* Author: Thomas Levine
 
 */
+
+(function(){
+  var datestamp = document.URL.replace(
+    /.*(\d{4}-\d{2}-\d{2}).*/,
+    function(junk, datestamp){return datestamp}
+  );
+  alert(datestamp);
+})()
