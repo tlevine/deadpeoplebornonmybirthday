de0afe5a30c4235376e734aa235f93315ee79cc8
Thomas Levine <occurrence@thomaslevine.com>
2012-06-17 17:44:02 -0400

    rename "api"

--- www/content/js/_main.js
+++ www/content/js/_main.js
@@ -2,10 +2,31 @@
 
 */
 
-(function(){
-  var datestamp = document.URL.replace(
-    /.*(\d{4}-\d{2}-\d{2}).*/,
-    function(junk, datestamp){return datestamp}
-  );
-  alert(datestamp);
-})()
+
+
+$(function(){
+  var death = {};
+
+  // Get datestamp from URL
+  death.DATESTAMP = document.URL.replace(
+      /.*(\d{4}-\d{2}-\d{2}).*/,
+      function(junk, datestamp){return datestamp}
+    );
+  if (death.DATESTAMP===document.URL){
+    death.DATESTAMP = null
+  };
+
+  // Render the page
+  death.render_dead_people = function(datestamp){
+    // Only do something if there is a datestamp.
+    if (datestamp != null){
+      alert(datestamp);
+    };
+  };
+
+  death.main = function(){
+    death.render_dead_people(death.DATESTAMP);
+  }
+
+  death.main()
+})
