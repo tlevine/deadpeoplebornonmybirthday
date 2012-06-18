b59425ac44dd50869cb55486b588bb6ceda7f822
Thomas Levine <occurrence@thomaslevine.com>
2012-06-17 21:21:05 -0400

    plot dummy

--- www/content/js/_main.js
+++ www/content/js/_main.js
@@ -20,15 +20,7 @@ window.death = (function(){
     'first', 'middle', 'last', 'died', 'age', 'date_of_death', 'state'
   ];
 
-  // Row template
-  death.table_row_template = '<tr><td>%(' +
-    death.table_keys.join(')s</td><td>%(') +
-    ')s</td></tr>'
-  death.table_row_template = death.table_row_template.replace(
-    '%(age)s', '%(age)d').replace(
-    '<td>%(date_of_death)s','<td class="date_of_death">%(date_of_death)s'
-  );
-
+  // Table-writer
   death.table = function(people){
     var rows = people.map(function(person){
       var died = new Date(person.date_of_death);
@@ -41,15 +33,28 @@ window.death = (function(){
     $('#deadpeople').dataTable();
   };
 
-  death.plot = function(d){
-    console.log(d);
+  // Table row template
+  death.table_row_template = '<tr><td>%(' +
+    death.table_keys.join(')s</td><td>%(') +
+    ')s</td></tr>'
+  death.table_row_template = death.table_row_template.replace(
+    '%(age)s', '%(age)d').replace(
+    '<td>%(date_of_death)s','<td class="date_of_death">%(date_of_death)s'
+  );
+
+  // Plot
+  death.plot = function(people){
+    console.log(people);
   };
 
   // Render the page
   death.render_dead_people = function(datestamp){
     // Only do something if there is a datestamp.
     if (datestamp != null){
-      $.get('/data/people/' + death.DATESTAMP + '.json', death.table);
+      $.get('/data/people/' + death.DATESTAMP + '.json', function(people){
+        death.table(people);
+        death.plot(people);
+      });
 //    $.get('/data/counts/' + death.DATESTAMP + '.json', death.plot);
     };
   };
