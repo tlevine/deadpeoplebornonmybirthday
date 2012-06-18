3b72e3e3dfd21b119e7011d9e04f12e062531b41
Thomas Levine <occurrence@thomaslevine.com>
2012-06-18 01:21:05 -0400

    aoeu

--- data/people.py
+++ data/people.py
@@ -1,10 +1,21 @@
 import pymongo
+from copy import copy
+import datetime
+
+m = pymongo.Connection().middlenames.person
 
 def people(birthday):
     'Generate a list of people for a given birthday'
-
+    out = []
+    for person in m.find({'date_of_birth': birthday}):
+        out.append({
+        })
 
 def main():
-    m = pymongo.Connection().middlenames.person
     m.ensure_index('born_date')
     min_birthday = m.find().sort('date_of_birth').limit(1)
+    max_birthday = datetime.date.today()
+    i = copy(min_birthday)
+    while i < max_birthday:
+        i += datetime.timedelta(days=1)
+        people(i)
