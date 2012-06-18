f15c59438dbae5bc0ac185c0c35541cfae1914e9
Thomas Levine <occurrence@thomaslevine.com>
2012-06-17 21:02:39 -0400

    table

--- www/content/js/_main.js
+++ www/content/js/_main.js
@@ -19,17 +19,22 @@ window.death = (function(){
   death.table_keys = [
     'first', 'middle', 'last', 'died', 'age', 'date_of_death', 'state'
   ];
+
+  // Row template
   death.table_row_template = '<tr><td>%(' +
     death.table_keys.join(')s</td><td>%(') +
-    ')s</td></tr>';
+    ')s</td></tr>'
+  death.table_row_template = death.table_row_template.replace(
+    '%(age)s', '%(age)d').replace(
+    '<td>%(date_of_death)s','<td class="date_of_death">%(date_of_death)s'
+  );
+
   death.table = function(people){
     var rows = people.map(function(person){
       var died = new Date(person.date_of_death);
       person.died = died.toDateString();
-      person.age = Math.floor(
-        (person.died - new Date(death.DATESTAMP))
-        /31536000000
-      )
+      person.age = Math.floor((died - new Date(death.DATESTAMP))/31536000000);
+      person.date_of_death = died.getTime();
       return sprintf(death.table_row_template, person)
     });
     $('#deadpeople > tbody').html(rows.join(''));
@@ -44,7 +49,7 @@ window.death = (function(){
     // Only do something if there is a datestamp.
     if (datestamp != null){
       $.get('/data/people/' + death.DATESTAMP + '.json', death.table);
-      $.get('/data/counts/' + death.DATESTAMP + '.json', death.plot);
+//    $.get('/data/counts/' + death.DATESTAMP + '.json', death.plot);
     };
   };
 
