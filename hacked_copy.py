7fd4016d75e0a4538e19e6dddc0ecc4ad9a464e9
Thomas Levine <occurrence@thomaslevine.com>
2012-06-28 05:39:16 -0400

    stnhaoeusht

--- hacked_copy.py
+++ hacked_copy.py
@@ -2,6 +2,7 @@
 
 import datetime
 import sys, os
+import dumptruck
 
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
@@ -9,26 +10,32 @@ from boto.s3.key import Key
 conn = S3Connection(sys.argv[1], sys.argv[2])
 b = conn.get_bucket('www.deadpeoplebornonmybirthday.com')
 
+os.system('rm /tmp/s3_copy_log.sqlite')
+dt = dumptruck.DumpTruck(dbname = '/tmp/s3_copy_log.sqlite')
+
 def upload(filename, birthday):
     # Test that it's a date.
     datetime.date(*map(int, birthday.replace('18', '19').split('-')))
 
     k=Key(b)
-    k.key=birthday
-    k.content_type = 'text/html'
-    k.content_encoding = 'gzip'
+    k.key=os.path.join('data', 'bornon', birthday + '.json.gz')
+#   k.content_type = 'application/json'
+#   k.content_encoding = 'gzip'
     k.storage_class='REDUCED_REDUNDANCY'
+    k.set_metadata('Content-Type', 'application/json')
+    k.set_metadata('Content-Encoding', 'gzip')
 
     content = open(filename)
     k.set_contents_from_file(content)
     content.close()
+    k.close()
 
 def main():
     for jsongz in os.listdir(os.path.join('data', 'people')):
         filename = os.path.join('data', 'people', jsongz)
         birthday = jsongz.replace('.json.gz', '')
         upload(filename, birthday)
-        print filename, birthday
+        dt.insert({'filename': filename, 'birthday': birthday}, 'finished')
         break
 
 main()
